028e367e69dd7d02b0ab5ae99e086610ead9c431
Jamal Neufeld <me@jamal.io>
2016-09-10 16:42:27 -0400

    added coercion method, tests, build process

---
+++ src/Coerce.js
@@ -0,0 +1,133 @@
+const builtInTypes = {
+  'A': '[A-Z]',
+  '9': '[0-9]',
+  '*': '.',
+  '+': '.*',
+}
+
+export default class Coerce {
+  constructor({pattern, extend}) {
+    this.pattern = pattern;
+    this.extend = extend;
+    // Merge built in identifiers with custom ones
+    this.specialTypes = {
+      ...builtInTypes,
+      ...extend,
+    }
+    this.value = null;
+  }
+  
+  string(value) {
+    // Set up an array to push characters to
+    let chars = [];
+    
+    // Set up an array to push in whether the character was added because of a match or not
+    let matches = [];
+
+    let string = value;
+
+    let charArray = this.pattern.split('');
+    
+    // Here we loop through all of the characters in the pattern
+    for(let i = 0; i < charArray.length; i++) {
+      const a = charArray[i];
+      if(a in this.specialTypes) {
+
+        /*
+          If the pattern character is a "special type", 
+          meaning it has regex in the builtInTypes or the extend.
+          In this case it will try to find a match starting at
+          the beginning of the string. If a match is found, it will 
+          push the match to chars (if the regex was conditional, and 
+          there is no character, it still returns a match, but will push '').
+          It then stores whether it was an actual matched character or not to
+          matches. And finally if it was an actual match, it removes the first
+          character of the string (since it's successfully been added to chars[])
+        */
+        const regexp = new RegExp('^'+this.specialTypes[a], 'i');
+        const match = string.match(regexp);
+        chars.push(match);
+        if(match && match[0] !== '') {
+          matches.push(true);
+          string = string.slice(match.length);
+        } else {
+          matches.push(false);
+        }
+
+        // Everything below is dealing with cases where there's no regex for this character
+      } else if(a !== string[0]) {
+        if(string[0] !== '' && !!string[0]){
+          /* 
+            ^ if we're currently not at the end of the input string, and the next 
+            character in the string does not equal the next character in the pattern, 
+            cycle through the rest of the characters in the string. So long as none are
+            equal to the current pattern, put them at the end of the string, if one matches
+            push it to our chars[], remove it from our reference string, mark match as true, 
+            and stop cycling through the reference string.
+          */
+          let match = false;
+          for(let b = 0; b < string.length; b++) {
+            if(a !== string[0]) {
+              string = string.slice(1) + string[0];
+            } else {
+              chars.push(string[0]);
+              matches.push(true);
+              string = string.slice(1);
+              match = true;
+              break;
+            }
+          }
+          /*
+            This next block makes it such that if there's a bunch of characters
+            in sequence that aren't regex, they don't need to be manually added by the user/source
+            if a string is provided that reaches this point, it will automatcially 
+            add the characters so long as it doesn't hit another specialType
+          */
+          let shouldPush;
+          if(charArray[i-1] in this.specialTypes) {
+            shouldPush = chars[chars.length - 1];
+          } else {
+            shouldPush = true;
+          }
+          /*
+            So if the for loop above didn't find any matches to this character, and the 
+            previous character is not a specialType, then push it, marking this push as a
+            false match (since it's not in our reference string). Otherwise break out of the
+            containing for loop.
+          */
+          if(!match && shouldPush) {
+              chars.push(a);
+              matches.push(false);
+          } else if(!shouldPush) {
+            break;
+          }
+        } else {
+          /*
+            This section is where those extra characters that were added to the end of our
+            reference string get put back in instead of being lost because they didn't match.
+            If we're not at the first character (i-1 exists) and we are the last, then push (with
+            a false match since it wasn't a character match). Within the class we also track the 
+            value, so it it's called again (presumably someone typing), if they remove a character
+            rather than add on, we won't auto-append the characters (which would make backspacing 
+            impossible)
+          */
+          const isDelete = this.value && this.value.length > value.length;
+          if(!isDelete && charArray[i-1] && charArray[i-1] === chars[chars.length-1]) {
+            chars.push(a);
+            matches.push(false);
+          }
+        }
+      } else {
+        /* 
+          Here we know that there was a match, push to char[] and remove the character
+          from the reference string
+        */
+        chars.push(string[0]);
+        matches.push(true);
+        string = string.slice(1);
+      }
+    }
+    this.value = chars.join('');
+    return this.value;
+  }
+}
---
+++ tests/StringMatching.js
@@ -0,0 +1,186 @@
+// npm modules
+import expect from "expect";
+
+// test target with 2 versions, one es6 source and bundled 
+import CoerceString from "../src/Coerce";
+var CoerceStringBundled = require("../").default;
+
+runTests("ES6", CoerceString);
+runTests("Bundled", CoerceStringBundled);
+
+function runTests(name, CoerceStringClass) {
+    describe(`${name} - The string mask`, ()=>{
+
+        it("should fill in the pattern until it finds a match", () => {
+            const coerce = new CoerceStringClass({ pattern: "hello" });
+            expect(
+              coerce.string("lo")
+            ).toEqual("hello");
+        });
+
+        it("should fill in the pattern until it finds a match", () => {
+            const coerce = new CoerceStringClass({ pattern: "hello99" });
+            expect(
+              coerce.string("23")
+            ).toEqual("hello23");
+        });
+
+        it("should fill in the pattern even if there isn't a match", () => {
+            const coerce = new CoerceStringClass({ pattern: "hello" });
+            expect(
+              coerce.string("5")
+            ).toEqual("hello");
+        });
+
+        it("should fill in the pattern after all matches have been found", () => {
+            const coerce = new CoerceStringClass({ pattern: "hello" });
+            expect(
+              coerce.string("hel")
+            ).toEqual("hello");
+        });
+
+        it("should fill in the pattern even if there isn't a match", () => {
+            const coerce = new CoerceStringClass({ pattern: "hello" });
+            expect(
+              coerce.string("z")
+            ).toEqual("hello");
+        });
+
+        it("shouldn't add characters proceeding the pattern", () => {
+            const coerce = new CoerceStringClass({ pattern: "hello" });
+            expect(
+              coerce.string("hello5")
+            ).toEqual("hello");
+        });
+
+        it("should allow for repeating at the end of the pattern", () => {
+            const coerce = new CoerceStringClass({ pattern: "as+" });
+            expect(
+              coerce.string("ashello5df")
+            ).toEqual("ashello5df");
+        });
+
+        it("should move characters that don't match to the closest spot they match the pattern", () => {
+            const coerce = new CoerceStringClass({ pattern: "http://+" });
+            expect(
+              coerce.string("htAtp://")
+            ).toEqual("http://A");
+        });
+
+        it("should move characters that don't match to the closest spot they match the pattern", () => {
+            const coerce = new CoerceStringClass({ pattern: "http://Az" });
+            expect(
+              coerce.string("htAtp://")
+            ).toEqual("http://A");
+        });
+
+        it("should not move characters that don't match, and continue filling out the pattern", () => {
+            const coerce = new CoerceStringClass({ pattern: "http://Az" });
+            expect(
+              coerce.string("htAbtp://")
+            ).toEqual("http://Az");
+        });
+
+        it("should continue filling in the pattern with matches, not losing any characters when they don't match", () => {
+            const coerce = new CoerceStringClass({ pattern: "(999) 999-9999" });
+            expect(
+              coerce.string("2342342344")
+            ).toEqual("(234) 234-2344");
+        });
+
+        it("should continue filling in the pattern until the source string runs out", () => {
+            const coerce = new CoerceStringClass({ pattern: "(999) 999-9999" });
+            expect(
+              coerce.string("2342")
+            ).toEqual("(234) 2");
+            expect(
+              coerce.string("2342")
+            ).toNotEqual("(234) 2-");
+        });
+
+        it("should not fill in the pattern when there aren't characters in the string", () => {
+            const coerce = new CoerceStringClass({ pattern: "(999) 999-9999" });
+            expect(
+              coerce.string("")
+            ).toEqual("");
+            expect(
+              coerce.string("")
+            ).toNotEqual("() -");
+        });
+
+        it("should allow for custom special characters", () => {
+            const coerce = new CoerceStringClass({ pattern: "httpA", extend: {A: "a"} });
+            expect(
+              coerce.string("httpa")
+            ).toEqual("httpa");
+        });
+
+        it("should allow for conditional characters and skip when there's no match", () => {
+            const coerce = new CoerceStringClass({ pattern: "httpS://", extend: {S: "s?"} });
+            expect(
+              coerce.string("https")
+            ).toEqual("https");
+            expect(
+              coerce.string("httpa")
+            ).toEqual("http://");
+        });
+
+        it("should move string characters that don't match conditional characters further down the string", () => {
+            const coerce = new CoerceStringClass({ pattern: "httpS://+", extend: {S: "s?"} });
+            expect(
+              coerce.string("httpa")
+            ).toEqual("http://a");
+        });
+
+        it("should not continue when there's a conditional match", () => {
+            const coerce = new CoerceStringClass({ pattern: "httpS://+", extend: {S: "s?"} });
+            expect(
+              coerce.string("https")
+            ).toEqual("https");
+        });
+
+        it("should move conditional failures down the string regardless of type", () => {
+            const coerce = new CoerceStringClass({ pattern: "httpS://+", extend: {S: "s?"} });
+            expect(
+              coerce.string("http234")
+            ).toEqual("http://234");
+        });
+
+        it("should fill in the proceeding non-special character when there isn't a match", () => {
+            const coerce = new CoerceStringClass({ pattern: "(999) 999-9999" });
+            expect(
+              coerce.string("a")
+            ).toEqual("(");
+        });
+
+        it("should continue filling in the pattern with matches, not losing any characters when they don't match", () => {
+            const coerce = new CoerceStringClass({ pattern: "99/99/9999" });
+            expect(
+              coerce.string("23232344")
+            ).toEqual("23/23/2344");
+        });
+
+        it("should continue filling in the pattern until the source string runs out", () => {
+            const coerce = new CoerceStringClass({ pattern: "99/99/9999" });
+            expect(
+              coerce.string("234")
+            ).toEqual("23/4");
+        });
+
+        it("should allow removing characters after adding them", () => {
+            const coerce = new CoerceStringClass({ pattern: "(999) 999-9999" });
+            expect(
+              coerce.string("2345")
+            ).toEqual("(234) 5");
+            expect(
+              coerce.string("(234) ")
+            ).toEqual("(234) ");
+            expect(
+              coerce.string("(234)")
+            ).toEqual("(234)");
+            expect(
+              coerce.string("(234")
+            ).toEqual("(234");
+        });
+    });
+}
